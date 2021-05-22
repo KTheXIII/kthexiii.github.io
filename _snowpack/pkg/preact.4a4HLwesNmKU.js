8f465c6b4e30b916aefaa9bc19fdeba4a9f4aee6
KTheXIII <Pratchaya.K.X@Live.com>
2021-05-22 20:04:56 +0000

    Deploying to gh-pages from @ KTheXIII/kthexiii.github.io@bd10d13bffeb95bfb68c8b3d7f48535c5b5bfd65 🚀

---
+++ _snowpack/pkg/preact.4a4HLwesNmKU.js
@@ -0,0 +1,1 @@
+export{a as h,N as render}from"./common/preact.module-06e62caf.nz2wWDitA5vL.js";
---
+++ static/App.Kt4v779oauSa.js
@@ -0,0 +1,1 @@
+import{h as p}from"../_snowpack/pkg/preact.4a4HLwesNmKU.js";export const App=()=>p("div",{className:"app"},p("h1",null,"I'm here."));
