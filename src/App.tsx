dbedf4a4c59d31b6f49087799989b58d3a26220b
KTheXIII <pratchaya.k.git@gmail.com>
2021-05-30 21:41:15 +0200

    Transition animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { h, FunctionalComponent } from 'preact'
+import { useEffect, useRef } from 'preact/hooks'
 
 export const App: FunctionalComponent = () => {
+  const helloRef = useRef<HTMLHeadingElement | null>(null)
+  let timeoutID: number
+
+  useEffect(() => {
+    if(!timeoutID)
+      timeoutID = setTimeout(() => {
+        helloRef.current?.classList.remove('invisible')
+        helloRef.current?.classList.add('animation-500ms-ease-in')
+      }, 250);
+  }, [helloRef])
+
   return (
     <div className="app">
-      <h1>I'm here.</h1>
+      <h1 ref={helloRef} className="invisible">I'm here.</h1>
     </div>
   )
 }
