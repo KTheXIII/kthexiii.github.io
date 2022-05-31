eb97cdd1984e6c69c4a65e3213084da16cea7851
mnerv <Pratchaya.K.X@Live.com>
2022-05-31 15:08:55 +0000

    Deploying to gh-pages from @ mnerv/mnerv.github.io@8acb1afbc5f16cff2c74e7468b8bfe8823f86518 🚀

--- snowpack/env.js
+++
@@ -1,1 +0,0 @@
-export const APP_NAME="kthexiii.github.io",VERSION="0.0.0",AUTHOR=void 0,DESCRIPTION="Personal website with github.io",HOMEPAGE=void 0,REPOSITORY_URL=void 0,COMMIT_HASH="253794a00a92ff1d337e9362b27155f9744f46e3",PUBLIC_URL="./",MODE="production",NODE_ENV="production",SSR=!1;
